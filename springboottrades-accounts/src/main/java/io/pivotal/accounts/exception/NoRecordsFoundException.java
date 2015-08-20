5d5f66e0f997a023fc7f755db8b2fc11d0abe695
Saurabh Gupta <saurabhgupta.sg@gmail.com>
2015-08-20 14:04:06 -0400

    add exception class

--- springboottrades-accounts/src/main/java/io/pivotal/accounts/exception/NoRecordsFoundException.java
+++ springboottrades-accounts/src/main/java/io/pivotal/accounts/exception/NoRecordsFoundException.java
@@ -18,7 +18,7 @@ package io.pivotal.accounts.exception;
 /**
  *  NoRecordsFoundException should be thrown whenever an entity is not 
  *  found via the repositories finder method. This exception is thrown from the controller
- *  
+ *
  *  @author Brian Dussault
  */
 
